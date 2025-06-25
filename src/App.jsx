3b21d5bd6bb88dd60d141df7acc20c6b0298accf
Andhy Koba <andhyweslheykoba@gmail.com>
2025-06-25 11:08:58 +0100

    Change the first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 // App.jsx
-import { Button } from "@chakra-ui/react"
-import {
-  Box,
-  Image
-} from '@chakra-ui/react'
 import "./App.css"
-import { Link as RouterLink } from "react-router-dom"
+import ConnexionPage from "../src/pages/connexion.page.jsx";
 
 function App() {
   return (
-    <Box minH="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column" gap="20px" className="gradient">
-      <Image
-      boxSize='100px'
-      objectFit='cover'
-      src='src/assets/AUSG logo blanc.png'
-      alt='AUSG logo'
-      />
-      <Box display="flex" flexDirection="column" gap="10px" w="300px" p="40px" bg="white" borderRadius="10px" boxShadow="lg">
-        <RouterLink to="/connexion">
-          <Button colorScheme="brand" w="100%">Connexion</Button>
-        </RouterLink>
-        <RouterLink to="/inscription">
-          <Button colorScheme="brand" w="100%">Inscription</Button>
-        </RouterLink>
-      </Box>
-    </Box>
+    <ConnexionPage />
   )
 }
 
